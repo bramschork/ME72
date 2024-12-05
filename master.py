89d0de555d1cac18df1e76ecc3abd4cf45588db5
Bram Schork <bram@bramschork.com>
2024-12-05 03:34:29 -0800

    r2n

--- master.py
+++ master.py
@@ -36,26 +36,27 @@ while True:
         if left_y > 0.2:   # Forward
             bus.write_byte(addr, 0x14)  # 20
             time.sleep(0.1)
+            print('ONE FORWARD')
         elif left_y < -0.2:   # Reverse
             bus.write_byte(addr, 0x7E4)  # (20)20
             time.sleep(0.1)
-            # print('REVERSE')
+            print('ONE REVERSE')
         else:  # Zero
             bus.write_byte(addr, 0x0)  # 0
             time.sleep(0.1)
-            # print('L0')
+            print('ONE ZERO')
 
         if right_y > 0.2:   # Forward
             bus.write_byte(addr, 0x3FC)  # 1020
             time.sleep(0.1)
-            # print('1020')
+            print('TWO FORWARD')
         elif right_y < -0.2:   # Reverse
             bus.write_byte(addr, 0xBCC)  # (30)20
             time.sleep(0.1)
-            # print('3020')
+            print('TWO REVERSE')
         else:  # Zero
             bus.write_byte(addr, 0x3E8)  # 1000
             time.sleep(0.1)
-            print('ZERO')
+            print('TWO ZERO')
     else:
         print("No joystick connected.")
