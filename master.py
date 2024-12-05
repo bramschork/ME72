1caea97cf7e0d1cec8969527a1b11ebbb57fec5c
Bram Schork <bram@bramschork.com>
2024-12-05 02:16:58 -0800

    2hdiu

--- master.py
+++ master.py
@@ -45,21 +45,21 @@ while True:
         else:
             R_modifier = 30
 
-        # print(f'Modifier: {modifier} Left_y: {left_y}')
+        ''' # print(f'Modifier: {modifier} Left_y: {left_y}')
         if left_y > 0.2:   # Forward
             bus.write_byte(addr, L_modifier)
             print(L_modifier)
         else:
             bus.write_byte(addr, 0)
-            print('L0')
+            print('L0')'''
 
-        '''if right_y > 0.2:   # Forward
+        if right_y > 0.2:   # Forward
             bus.write_byte(addr, R_modifier+1000)
             print(R_modifier)
         else:
             bus.write_byte(addr, 1000)
             print('R0')
 
-        time.sleep(0.2)'''
+        time.sleep(0.2)
     else:
         print("No joystick connected.")
