4bfe8d0790ad17c0ecd26e1dfe5bc115dc8d2eb6
Bram Schork <bram@bramschork.com>
2024-12-04 22:53:47 -0800

    test

--- master.py
+++ master.py
@@ -38,9 +38,8 @@ while True:
         else:  # Forward
             bus.write_byte(addr, hex(int(left_y)*modifier))
             print(hex(int(left_y)*modifier))'''
-    print(type(left_y))
-    bus.write_byte(addr, hex(left_y))
-    print(hex(left_y))
+    bus.write_byte(addr, left_y.hex())
+    print(left_y.hex())
     # else:
     #    bus.write_byte(addr, 0x0)
 
