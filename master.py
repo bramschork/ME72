1ccec8c58470d39e0979551d984ed14c9d549273
Bram Schork <bram@bramschork.com>
2024-12-04 22:57:39 -0800

    nf42o

--- master.py
+++ master.py
@@ -38,8 +38,8 @@ while True:
         else:  # Forward
             bus.write_byte(addr, hex(int(left_y)*modifier))
             print(hex(int(left_y)*modifier))'''
-    print(hex(int(modifier)))
-    bus.write_byte(addr, hex(int(modifier)))
+    bus.write_byte(addr, 0x0)
+    print(type(modifier))
     # else:
     #    bus.write_byte(addr, 0x0)
 
