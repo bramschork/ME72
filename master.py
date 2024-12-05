579ec30912a828fcd2a0776073b81ce57d20faf9
Bram Schork <bram@bramschork.com>
2024-12-05 00:35:36 -0800

    test

--- master.py
+++ master.py
@@ -17,30 +17,33 @@ bus = SMBus(1)  # indicates /dev/ic2-1
 #    pygame.joystick.Joystick.get_power_level))
 
 while True:
-    left_stick = pygame.joystick.Joystick(0)
-    left_stick.init()
+    if pygame.joystick.get_count() > 0:
+        left_stick = pygame.joystick.Joystick(0)
+        left_stick.init()
 
-    right_stick = pygame.joystick.Joystick(0)
-    right_stick.init()
+        right_stick = pygame.joystick.Joystick(0)
+        right_stick.init()
 
-    # print(f"Connected to: {left_stick.get_name()}")
-    pygame.event.pump()
+        # print(f"Connected to: {left_stick.get_name()}")
+        pygame.event.pump()
 
-    # Left joystick axes for PS4 controller
-    left_y = left_stick.get_axis(1)  # Vertical axis (inverted)
-    right_y = right_stick.get_axis(3)  # Vertical axis (inverted)
+        # Left joystick axes for PS4 controller
+        left_y = left_stick.get_axis(1)  # Vertical axis (inverted)
+        right_y = right_stick.get_axis(3)  # Vertical axis (inverted)
 
-    # MODIFIER to slow down the motors
-    modifier = (round(.5 * 127 * left_y))
+        # MODIFIER to slow down the motors
+        modifier = (round(.5 * 127 * left_y))
 
-    if left_y > 0.2:  # Reverse
-        bus.write_byte(addr, modifier+1000)
-    elif left_y < 0.2:  # Forward
-        bus.write_byte(addr, modifier)
+        if left_y > 0.2:  # Reverse
+            bus.write_byte(addr, modifier+1000)
+        elif left_y < 0.2:  # Forward
+            bus.write_byte(addr, modifier)
 
-    if right_y > 0.2:  # Reverse
-        bus.write_byte(addr, modifier+3000)
-    elif right_y < 0.2:  # Forward
-        bus.write_byte(addr, modifier+2000)
+        if right_y > 0.2:  # Reverse
+            bus.write_byte(addr, modifier+3000)
+        elif right_y < 0.2:  # Forward
+            bus.write_byte(addr, modifier+2000)
 
-    time.sleep(0.1)
+        time.sleep(0.1)
+    else:
+        print("No joystick connected.")
