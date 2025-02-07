e77ba8a1dbfe1af0204dff6e8a680ec5fc0768b3
Bram Schork <bram@bramschork.com>
2025-02-07 14:46:09 -0800

    servo test

---
+++ Tests/servo_test.py
@@ -0,0 +1,11 @@
+from gpiozero import Servo
+from time import sleep
+
+# Servo connected to GPIO 20 (MISO)
+servo = Servo(20, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+
+while True:
+    servo.min()  # Move to 0 degrees
+    sleep(1)
+    servo.max()  # Move to 180 degrees
+    sleep(1)
