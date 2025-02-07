b8303253b14a6981807564f348632a20c37b3404
Bram Schork <bram@bramschork.com>
2025-02-07 15:52:29 -0800

    change

--- Tests/single_robowclaw_test_packet_serial.py
+++ Tests/single_robowclaw_test_packet_serial.py
@@ -3,7 +3,7 @@ from time import sleep
 
 controller_address_1 = 0x80  # 128 in hex
 # controller_address_2 = 0x82  # 128 in hex
-roboclaw_1 = Roboclaw("/dev/ttyS0", 115200)
+roboclaw = Roboclaw("/dev/ttyS0", 115200)
 roboclaw.Open()
 
 while True:
