cf6517d4059253c8f8f9f4c846defc62c06d6ea1
Bram Schork <bram@bramschork.com>
2025-02-07 16:04:19 -0800

    changes

--- Tests/servo_test.py
+++ Tests/servo_test.py
@@ -1,8 +1,8 @@
 from gpiozero import Servo
 from time import sleep
 
-# Servo connected to GPIO 20 (MISO)
-servo = Servo(20, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+# Eighth down on right pins facing right
+servo = Servo(16, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
 while True:
     servo.min()  # Move to 0 degrees
