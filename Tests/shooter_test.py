567c9b8f6a0b030b85f1c079194dc1222a6ff63d
Bram Schork <bram@bramschork.com>
2025-02-11 11:59:55 -0800

    servo test

--- Tests/shooter_test.py
+++ Tests/shooter_test.py
@@ -1,19 +1,23 @@
 from roboclaw_3 import Roboclaw
+from gpiozero import Servo
 from time import sleep
-address = 0x82  # 128 in hex
+address = 0x82  # 130 in hex
+
 # controller_address_2 = 0x82  # 128 in hex
 roboclaw = Roboclaw("/dev/ttyS0", 460800)
 roboclaw.Open()
 
+# Second up from bottom pins facing right
+servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+
 while True:
+    servo.min()  # Move to 0 degrees
+    roboclaw.ForwardM1(address, 20)
+    roboclaw.ForwardM2(address, 20)
+    sleep(1)
 
-    print('Start')
-    roboclaw.ForwardM1(address, 64)
-    sleep(2)
-    roboclaw.ForwardM1(address, 0)
-    sleep(2)
+    servo.max()  # Move to 180 degre
+    roboclaw.ForwardM1(address, 20)
+    roboclaw.ForwardM2(address, 20)
 
-    roboclaw.ForwardM2(address, 64)
-    sleep(2)
-    roboclaw.ForwardM2(address, 0)
-    sleep(2)
+    sleep(1)
