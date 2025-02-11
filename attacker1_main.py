c20a63cebf5da10312fc9f2a444c12a35e1d3b55
Bram Schork <bram@bramschork.com>
2025-02-11 11:55:07 -0800

    servo

--- attacker1_main.py
+++ attacker1_main.py
@@ -161,7 +161,7 @@ def shooter_motor_control():
             time.sleep(2)  # Simulate ramp-up time
             print("Shooter Motors Up to Speed!")
 
-            servo = Servo(20, min_pulse_width=0.5/1000,
+            servo = Servo(12, min_pulse_width=0.5/1000,
                           max_pulse_width=2.5/1000)
 
             servo.min()  # Move to 0 degrees
