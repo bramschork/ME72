f48349ca660463062162169da494daf1bc4e419a
Bram Schork <bram@bramschork.com>
2025-02-11 13:15:40 -0800

    pus

--- Tests/shooter_test.py
+++ Tests/shooter_test.py
@@ -30,15 +30,14 @@ def find_ps4_controller():
 def trigger_pulled():
     # Shooting
     servo.max()
-    roboclaw.BackwardM1(address, 20)
-    roboclaw.BackwardM2(address, 20)
+    roboclaw.ForwardM1(address, 20)
+    roboclaw.ForwardM2(address, 20)
     sleep(1)
-    time.sleep(1)
     servo.min()
 
     # Back to intake
-    roboclaw.ForwardM1(address, 20)
-    roboclaw.ForwardM2(address, 20)
+    roboclaw.BackwardM2(address, 20)
+    roboclaw.BackwardM2(address, 20)
 
 # Main loop to poll the trigger
 
@@ -54,8 +53,8 @@ def poll_trigger():
 
 
 # Set to intake
-roboclaw.ForwardM1(address, 20)
-roboclaw.ForwardM2(address, 20)
+roboclaw.BackwardM1(address, 20)
+roboclaw.BackwardM2(address, 20)
 servo.min()  # Move to 0 degrees
 
 
