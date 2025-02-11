3971e3bd7f6964a8677405457bc2367def7a8ddf
Bram Schork <bram@bramschork.com>
2025-02-11 11:48:58 -0800

    add all

--- Tests/servo_test.py
+++ Tests/servo_test.py
@@ -2,7 +2,7 @@ from gpiozero import Servo
 from time import sleep
 
 # Second up from bottom pins facing right
-servo = Servo(20, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
 while True:
     servo.min()  # Move to 0 degrees
