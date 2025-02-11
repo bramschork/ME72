ace76a6baf84d49489c89c25d4616204b193ae2e
Bram Schork <bram@bramschork.com>
2025-02-10 21:39:51 -0800

    baud

--- Tests/dual_roboClaw_test_packet_serial.py
+++ Tests/dual_roboClaw_test_packet_serial.py
@@ -6,8 +6,8 @@ controller_address_1 = 0x80  # 128 in hex
 controller_address_2 = 0x82  # 130 in hex
 
 # Initialize both Roboclaws
-roboclaw_1 = Roboclaw("/dev/ttyS0", 115200)
-roboclaw_2 = Roboclaw("/dev/ttyS0", 115200)
+roboclaw_1 = Roboclaw("/dev/ttyS0", 460800)
+roboclaw_2 = Roboclaw("/dev/ttyS0", 460800)
 
 # Open serial communication
 roboclaw_1.Open()
--- Tests/single_robowclaw_test_packet_serial.py
+++ Tests/single_robowclaw_test_packet_serial.py
@@ -3,7 +3,7 @@ from time import sleep
 
 address = 0x80  # 128 in hex
 # controller_address_2 = 0x82  # 128 in hex
-roboclaw = Roboclaw("/dev/ttyS0", 115200)
+roboclaw = Roboclaw("/dev/ttyS0", 460800)
 roboclaw.Open()
 
 while True:
