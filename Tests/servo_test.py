8410104600d1f7d2d535c609213cc9ff3b56a525
Bram Schork <bram@bramschork.com>
2025-02-27 01:50:51 -0800

    push

--- Tests/servo_test.py
+++ Tests/servo_test.py
@@ -5,7 +5,9 @@ from time import sleep
 servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
 while True:
-    servo.min()  # Move to 0 degrees
+    # servo.min()  # Move to 0 degrees
+    servo.value = 0
     sleep(1)
-    servo.max()  # Move to 180 degrees
+    # servo.max()  # Move to 180 degrees
+    servo.value = 180
     sleep(1)
