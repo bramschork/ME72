f240e5f23e08ce16ad366e683daea1e99a4bbab2
Bram Schork <bram@bramschork.com>
2025-02-27 15:37:40 -0800

    push

--- attack2.py
+++ attack2.py
@@ -206,8 +206,8 @@ def poll_joystick(controller):
             elif event.type == ecodes.EV_KEY:
                 # Print "L1" when the L1 button is pressed
                 if event.code == ecodes.BTN_TL and event.value == 1:
-                    shooter_roboclaw.BackwardM1M1(shooter_address, 32)
-                    shooter_roboclaw.BackwardM1(shooter_address, 32)
+                    shooter_roboclaw.BackwardM1M1(shooter_address, 16)
+                    shooter_roboclaw.BackwardM1(shooter_address, 16)
                     # print('L1')
 
                     # Start the timer thread with a delay of, for example, 5 seconds
